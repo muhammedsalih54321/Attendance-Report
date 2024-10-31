bb8a127695d9c22c1548e801636c38b775870d2c
hunaisp <hunaispc8@gmail.com>
2024-10-31 23:48:53 +0530

    overtime checkin and checkout added

--- lib/Repository/Api/HRM_Api/hrm_api.dart
+++ lib/Repository/Api/HRM_Api/hrm_api.dart
@@ -8,57 +8,65 @@ import 'package:pr_2/main.dart';
 
 import '../../Model/CheckInModel.dart';
 
-
 class HrmApi {
   ApiClient apiClient = ApiClient();
+
 //1
-  Future<LoginModel> getLogin(String email,String password ) async {
+  Future<LoginModel> getLogin(String email, String password) async {
     String trendingpath = '/auth/local/login';
 
     print(trendingpath);
-    var body ={
-    "email":email,
-    "password":password
-};
-print(body);
+    var body = {"email": email, "password": password};
+    print(body);
     Response response =
         await apiClient.invokeAPI(trendingpath, 'LOGINPOST', jsonEncode(body));
-       
-print(response.body);
+
+    print(response.body);
     return LoginModel.fromJson(jsonDecode(response.body));
   }
-  //2
- Future<ReportModel> getReport(String month,String year) async {
-    String trendingpath = '/api/attendances/report?month=$month&year=$year';
-    var body = {
 
-    };
+  //2
+  Future<ReportModel> getReport(String month, String year) async {
+    String trendingpath = '/api/attendances/report?month=$month&year=$year';
+    var body = {};
     Response response = await apiClient.invokeAPI(trendingpath, 'GET', body);
 
     return ReportModel.fromJson(jsonDecode(response.body));
   }
- //check in api
+
+  //check in api
   Future<CheckInModel> checkIn(String qr) async {
     String trendingpath = '/api/attendances/check-in/$qr';
 
     print(trendingpath);
-    var body ={
-
-    };
+    var body = {};
     Response response = await apiClient.invokeAPI(trendingpath, 'POST', body);
 
     return CheckInModel.fromJson(jsonDecode(response.body));
   }
+
   //checkout api
-  Future<void> checkOut(String qr,String attendenceId) async {
+  Future<void> checkOut(String qr, String attendenceId) async {
     String trendingpath = '/api/attendances/check-out/$attendenceId/$qr';
 
     print(trendingpath);
-    var body ={
-
-    };
+    var body = {};
     await apiClient.invokeAPI(trendingpath, 'POST', body);
+  }
+  //overTime CheckIn
+  Future<void> overTimeCheckIn(String qr, String attendenceId) async {
+    String trendingpath = '/api/attendances/overtime-check-in/$attendenceId/$qr';
 
+    print(trendingpath);
+    var body = {};
+    await apiClient.invokeAPI(trendingpath, 'POST', body);
+  }
+  //overTime CheckOut
+  Future<void> overTimeCheckOut(String qr, String attendenceId) async {
+    String trendingpath = '/api/attendances/overtime-check-out/$attendenceId/$qr';
 
+    print(trendingpath);
+    var body = {};
+    await apiClient.invokeAPI(trendingpath, 'POST', body);
   }
 }

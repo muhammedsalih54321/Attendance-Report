62f0b8303ee76ae92f29cf064195d5d32a9b6779
hunaisp <hunaispc8@gmail.com>
2024-10-31 20:21:19 +0530

    dd

--- lib/Ui/Home.dart
+++ lib/Ui/Home.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:bootstrap_icons/bootstrap_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,7 +8,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:pr_2/Ui/Attendance_details.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -16,16 +16,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  
   String Qrcoderesult = '';
-  var Time = DateTime.now();
+  DateTime currentTime = DateTime.now();
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    // Timer to update time every second
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => _updateTime());
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  void _updateTime() {
+    setState(() {
+      currentTime = DateTime.now();
+    });
+  }
 
   Future<void> QRscanner() async {
     String ResultData;
     try {
       ResultData = await FlutterBarcodeScanner.scanBarcode(
           "#ff6666", "cancel", true, ScanMode.QR);
-
 
       setState(() {
         Qrcoderesult = ResultData;
@@ -52,17 +70,19 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context, designSize: Size(411, 823));
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: _isCheckedin == true ? Color(0xFFFF6000) : Colors.pink,
+        backgroundColor: _isCheckedin ? Color(0xFFFF6000) : Colors.pink,
         automaticallyImplyLeading: false,
         title: Text(
-          'Attendence',
+          'Attendance',
           style: GoogleFonts.inter(
               color: Colors.white,
               fontSize: 20.sp,
               fontWeight: FontWeight.w600,
-              height: 0.10),
+              height: 1.2),
         ),
         actions: [
           GestureDetector(
@@ -105,200 +125,199 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 80.h,
-            ),
-            Center(
-              child: Container(
-                width: 300.w,
-                height: 100.h,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        '${Time.hour >= 12 ? '0' : ''}${Time.hour % 12 == 0 ? 12 : Time.hour % 12}:${Time.minute.toString().padLeft(2, '0')} ${Time.hour >= 12 ? 'PM' : 'AM'}',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 40,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 2),
-                    Text(
-                      '${DateFormat.MMMMEEEEd().format(Time)}',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Column(
+              children: [
+                SizedBox(
+                  height: 80.h,
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 30.h,
-            ),
-            GestureDetector(
-              onTap: () {
-                _isCheckedin ? QRscanner() : QRscanner();
-              },
-              child: CircleAvatar(
-                radius: 110.r,
-                backgroundColor:
-                    _isCheckedin == true ? Color(0xFFFF6000) : Colors.pink,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        BootstrapIcons.hand_index_thumb,
-                        color: Colors.white,
-                        size: 80.sp,
-                      ),
-                      SizedBox(
-                        height: 20.h,
-                      ),
-                      Text(
-                        _isCheckedin == true ? 'Check in' : 'Check out',
-                        style: GoogleFonts.inter(
-                            color: Colors.white,
-                            fontSize: 22.sp,
-                            fontWeight: FontWeight.w500,
-                            height: 0.10),
-                      ),
-                    ],
+                Center(
+                  child: Container(
+                    width: 300.w,
+                    height: 100.h,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            '${currentTime.hour >= 12 ? '0' : ''}${currentTime.hour % 12 == 0 ? 12 : currentTime.hour % 12}:${currentTime.minute.toString().padLeft(2, '0')} ${currentTime.hour >= 12 ? 'PM' : 'AM'}',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 40.sp,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          '${DateFormat.MMMMEEEEd().format(currentTime)}',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24.sp,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 80.h,
-            ),
-            Container(
-                width: double.infinity.w,
-                height: 100.h,
-                child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        onTap: checkIn,
-                        child: Container(
+                SizedBox(
+                  height: 30.h,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    QRscanner();
+                  },
+                  child: CircleAvatar(
+                    radius: 110.r,
+                    backgroundColor: _isCheckedin ? Color(0xFFFF6000) : Colors.pink,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            BootstrapIcons.hand_index_thumb,
+                            color: Colors.white,
+                            size: 80.sp,
+                          ),
+                          SizedBox(
+                            height: 20.h,
+                          ),
+                          Text(
+                            _isCheckedin ? 'Check in' : 'Check out',
+                            style: GoogleFonts.inter(
+                                color: Colors.white,
+                                fontSize: 22.sp,
+                                fontWeight: FontWeight.w500,
+                                height: 1.2),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 80.h,
+                ),
+                Container(
+                    width: double.infinity,
+                    height: 100.h,
+                    child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: checkIn,
+                            child: Container(
+                                width: 110.w,
+                                height: 100.h,
+                                child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        BootstrapIcons.clock,
+                                        color: _isCheckedin
+                                            ? Color(0xFFFF6000)
+                                            : Colors.pink,
+                                        size: 30.sp,
+                                      ),
+                                      Text(
+                                        '--:--',
+                                        style: GoogleFonts.inter(
+                                          color: Colors.black,
+                                          fontSize: 22.sp,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Check In',
+                                        style: GoogleFonts.inter(
+                                          color: Colors.grey,
+                                          fontSize: 15.sp,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ])),
+                          ),
+                          GestureDetector(
+                            onTap: checkOut,
+                            child: Container(
+                                width: 110.w,
+                                height: 100.h,
+                                child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        BootstrapIcons.clock,
+                                        color: _isCheckedin
+                                            ? Color(0xFFFF6000)
+                                            : Colors.pink,
+                                        size: 30.sp,
+                                      ),
+                                      Text(
+                                        '--:--',
+                                        style: GoogleFonts.inter(
+                                          color: Colors.black,
+                                          fontSize: 22.sp,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Check Out',
+                                        style: GoogleFonts.inter(
+                                          color: Colors.grey,
+                                          fontSize: 15.sp,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ])),
+                          ),
+                          Container(
                             width: 110.w,
                             height: 100.h,
                             child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    BootstrapIcons.clock,
-                                    color: _isCheckedin == true
-                                        ? Color(0xFFFF6000)
-                                        : Colors.pink,
-                                    size: 30.sp,
-                                  ),
-                                  Text(
-                                    '--:--',
-                                    style: GoogleFonts.inter(
-                                      color: Colors.black,
-                                      fontSize: 22.sp,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                  Text(
-                                    'Check In',
-                                    style: GoogleFonts.inter(
-                                      color: Colors.grey,
-                                      fontSize: 15.sp,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                ])),
-                      ),
-                      GestureDetector(
-                        onTap: checkOut,
-                        child: Container(
-                            width: 110.w,
-                            height: 100.h,
-                            child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    BootstrapIcons.clock,
-                                    color: _isCheckedin == true
-                                        ? Color(0xFFFF6000)
-                                        : Colors.pink,
-                                    size: 30.sp,
-                                  ),
-                                  Text(
-                                    '--:--',
-                                    style: GoogleFonts.inter(
-                                      color: Colors.black,
-                                      fontSize: 22.sp,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                  Text(
-                                    'Check Out',
-                                    style: GoogleFonts.inter(
-                                      color: Colors.grey,
-                                      fontSize: 15.sp,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                ])),
-                      ),
-                      Container(
-                          width: 110.w,
-                          height: 100.h,
-                          child: Column(
                               mainAxisSize: MainAxisSize.min,
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                                Icon(
-                                  BootstrapIcons.clock_history,
-                                  color: _isCheckedin == true
-                                      ? Color(0xFFFF6000)
-                                      : Colors.pink,
-                                  size: 30.sp,
-                                ),
-                                Text(
-                                  '--:--',
-                                  style: GoogleFonts.inter(
-                                    color: Colors.black,
-                                    fontSize: 22.sp,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                Text(
-                                  'Working Hr\'s',
-                                  style: GoogleFonts.inter(
-                                    color: Colors.grey,
-                                    fontSize: 15.sp,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ])),
-                    ])),
-          ],
-        ),
-      ),
+                              Icon(
+                              BootstrapIcons.clock_history,
+                              color: _isCheckedin
+                                  ? Color(0xFFFF6000)
+                                  : Colors.pink,
+                              size: 30.sp,
+                            ),
+                            Text(
+                              '--:--',
+                              style: GoogleFonts.inter(
+                                color: Colors.black,
+                                fontSize: 22.sp,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              'Working Hrs',
+                            style: GoogleFonts.inter(
+                            color: Colors.grey,
+                              fontSize: 15.sp,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ])),
+              ])),
+      ],
+    ),
+    ),
     );
   }
 }
